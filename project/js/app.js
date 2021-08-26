97534e27988c1f41d2661d8e040cc8a2bcbcd030
aungwinlatt <aungwinlatt1999@gmail.com>
2021-08-26 22:22:05 +0630

    second commit

--- project/js/app.js
+++ project/js/app.js
@@ -1,25 +1,86 @@
-$(document).ready(function () {
-  let screenHeight = $(window).height();
-  console.log(screenHeight);
+let screenHeight = $(window).height();
+// console.log(screenHeight);
 
-  $(window).scroll(function () {
-    let currentPosition = $(this).scrollTop();
-    console.log(currentPosition);
-    if (currentPosition >= screenHeight - 100) {
-      $(".site-nav").addClass("site-nav-scroll");
-    } else {
-      $(".site-nav").removeClass("site-nav-scroll");
-    }
-  });
+$(window).scroll(function () {
+  let currentPosition = $(this).scrollTop();
+  // console.log(currentPosition);
+  if (currentPosition >= screenHeight - 100) {
+    $(".site-nav").addClass("site-nav-scroll");
+  } else {
+    $(".site-nav").removeClass("site-nav-scroll");
+    setActive("home");
+  }
+});
 
-  $(".navbar-toggler").click(function () {
-    let result = $(".navbar-collapse").hasClass("show");
-    console.log(result);
+function setActive(current) {
+  $(".nav-link").removeClass("current-section");
 
-    if (result) {
-      $(".menu-icon").removeClass("fa-close").addClass("fa-bars");
-    } else {
-      $(".menu-icon").removeClass("fa-bars").addClass("fa-close");
-    }
+  $(`.nav-link[href='#${current}']`).addClass("current-section");
+}
+
+function navScroll() {
+  let currentSection = $("section[id]");
+  currentSection.waypoint(
+    function (direction) {
+      if (direction == "down") {
+        let currentSectionId = $(this.element).attr("id");
+        console.log(currentSectionId);
+        setActive(currentSectionId);
+      }
+    },
+    { offset: "150px" }
+  );
+
+  currentSection.waypoint(
+    function (direction) {
+      if (direction == "up") {
+        let currentSectionId = $(this.element).attr("id");
+        console.log(currentSectionId);
+        setActive(currentSectionId);
+      }
+    },
+    { offset: "150px" }
+  );
+}
+
+navScroll();
+
+new WOW().init();
+$(".pricing-slide").slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+$(window).on("load", function () {
+  $(".loader-container").fadeOut(500, function () {
+    $(this).remove();
   });
 });
